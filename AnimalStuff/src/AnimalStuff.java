1d4e516c637b8bbf7cabe0b82067285c000f117c
= <=>
2021-04-19 14:41:08 +0300

    AnimmalStuff changes made:
    Added input validation. Now only integers are accepted for speed input.
    all the names are now capitalized before use
    movement states are lowercased before their use in sentences

--- AnimalStuff/src/AnimalStuff.java
+++ AnimalStuff/src/AnimalStuff.java
@@ -44,12 +44,14 @@ public class AnimalStuff {
     }
 
     public static void printThings(Animal an) {
-        System.out.print(an.getName() + " is " + an.movement() + " while saying " + an.makeNoise() + '\n');
+        CapitalL cp = new CapitalL();
+        System.out.print(cp.capitalizer(an.getName()) + " is " + an.movement().toLowerCase() +
+                " while saying " + an.makeNoise() + '\n');
     }
 
     public static void setParams(Animal a, Animal b, Animal c) {
+        InputValidation validate = new InputValidation();
         Scanner name = new Scanner(System.in);
-        Scanner speed = new Scanner(System.in);
 
         a.setParameters();
         b.setParameters();
@@ -58,23 +60,24 @@ public class AnimalStuff {
         System.out.print("Set name for " + a.getSpecies() + " " + a.getTag() + ": ");
         a.setName(name.nextLine());
         System.out.print("Set speed for " + a.getName() + ": ");
-        a.setSpeed(speed.nextInt());
+        a.setSpeed(validate.inputValidate());
         System.out.print('\n');
 
         System.out.print("Set name for " + b.getSpecies() + " " + b.getTag() + ": ");
         b.setName(name.nextLine());
         System.out.print("Set speed for " + b.getName() +  ": ");
-        b.setSpeed(speed.nextInt());
+        b.setSpeed(validate.inputValidate());
         System.out.print('\n');
 
         System.out.print("Set name for " + c.getSpecies() + " " + c.getTag() + ": ");
         c.setName(name.nextLine());
         System.out.print("Set speed for " + c.getName() +  ": ");
-        c.setSpeed(speed.nextInt());
+        c.setSpeed(validate.inputValidate());
         System.out.print('\n');
 
         System.out.println("");
     }
+
 }
 
 abstract class Animal {
@@ -155,15 +158,18 @@ class Cat extends Animal {
 }
 
 class Comparison{
+    CapitalL ca = new CapitalL();
     public void speedCompIntra(Animal a, Animal b) {
         if (a.getSpeed() < b.getSpeed()) {
-            System.out.println(a.getName() + " is slower than " + b.getName());
+            System.out.println(ca.capitalizer(a.getName()) + " is slower than " + ca.capitalizer(b.getName()));
         } else if (a.getSpeed() == b.getSpeed()) {
-            System.out.println(a.getName() + " is the same speed as " + b.getName());
+            System.out.println(ca.capitalizer(a.getName()) + " is the same speed as " + ca.capitalizer(b.getName()));
         } else {
-            System.out.println(a.getName() + " is faster than " + b.getName());
+            System.out.println(ca.capitalizer(a.getName()) + " is faster than " + ca.capitalizer(b.getName()));
         }
     }
+
+
 
     public void speedCompInter(Animal a, Animal b) {
         if (a.getSpeed() < b.getSpeed()) {
@@ -176,5 +182,36 @@ class Comparison{
             System.out.println(a.getSpecies() + " " + a.getTag() + " is faster than " + b.getSpecies()+ " " +
                     b.getTag());
         }
+    }
+}
+
+class CapitalL {
+    public String capitalizer(String input) {
+        String capInput = input.substring(0, 1).toUpperCase() + input.substring(1);
+        return capInput;
+    }
+}
+
+class InputValidation {
+
+    public static int inputValidate() {
+        Scanner vs = new Scanner(System.in);
+        int input;
+
+        do {
+            while (!vs.hasNextInt()) {
+                System.out.println("Invalid input. Try again.");
+                vs.next();
+            }
+
+            input = vs.nextInt();
+
+            if (input < 0) {
+                System.out.println("Number must be at least 0. Try again.");
+            }
+
+        } while (input < 0);
+
+        return input;
     }
 }
